2adc89b306a248140b21c2168817968e652bf8f7
sohebcakewalk <soheb@cakewalk.in>
2015-04-12 01:56:32 +0530

    resolved conflicts
    resolved conflicts

--- myNotes/lib/collectons.js
+++ myNotes/lib/collectons.js
@@ -1,11 +1,3 @@
-
-<<<<<<< HEAD
 noteList = new Mongo.Collection('noteList');
 tagList = new Mongo.Collection('tagList');
 historyList= new Mongo.Collection('historyList');
-=======
-
-noteList = new Mongo.Collection('noteList');
-
-historyList= new Mongo.Collection('historyList')
->>>>>>> origin/master
