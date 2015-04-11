710c8d5430fc597cadceca04a3e767217d66f000
sohebcakewalk <soheb@cakewalk.in>
2015-04-11 19:38:11 +0530

    Login UI add fields
    Added loginAccount.js in loginAccount folder

---
+++ myNotes/client/loginAccount/loginAccount.js
@@ -0,0 +1,34 @@
+Accounts.ui.config({
+    requestPermissions: {},
+    extraSignupFields: [{
+        fieldName: 'first-name',
+        fieldLabel: 'First name',
+        inputType: 'text',
+        visible: true,
+        saveToProfile: true
+    }, {
+        fieldName: 'last-name',
+        fieldLabel: 'Last name',
+        inputType: 'text',
+        visible: true,
+        saveToProfile: true
+    },
+        /*{
+         fieldName: 'terms',
+         fieldLabel: 'I accept the terms and conditions',
+         inputType: 'checkbox',
+         visible: true,
+         saveToProfile: false
+         }*/]
+});
+
+accountsUIBootstrap3.logoutCallback = function(error) {
+
+
+    //Router.route('home', {name: 'home'});
+
+    Session.set('noteMode','addNote');
+    Session.set('noteId', '');
+
+
+}
